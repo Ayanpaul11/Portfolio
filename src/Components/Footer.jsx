54df79518608d05ecf5bdbccae29554e1a2955de
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-10-02 07:33:11 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import FCss from "./Css/Footer.module.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return <div className={FCss.FmDiv}>Footer</div>;
 }
