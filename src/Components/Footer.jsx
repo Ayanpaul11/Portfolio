a1e79d3d869ad37d1db04938ad2a4073e2dce872
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-10-02 07:22:45 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,5 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function Footer() {
-  return (
-    <div>
-      <LinkedInIcon />
-      Footer
-    </div>
-  );
+  return <div>Footer</div>;
 }
