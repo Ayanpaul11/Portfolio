8a692db50fbdc2dd877155c56a00e857f514b3ad
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 17:55:12 +0530

    Update Bio.jsx

--- src/Components/Home/Bio.jsx
+++ src/Components/Home/Bio.jsx
@@ -4,7 +4,7 @@ import BCss from "./Css/Bio.module.css";
 export default function Bio() {
   return (
     <div className={BCss.mDiv}>
-      <p className={BCss.pTagMain}></p>
+      <p className={BCss.pTagMain}>Me, Myself & I</p>
     </div>
   );
 }
