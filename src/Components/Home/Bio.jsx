12affdf83b6afb32010db510b8341afdfddb8cc2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 17:53:04 +0530

    Create Bio.jsx

---
+++ src/Components/Home/Bio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BCss from "./Css/Bio.module.css";
+
+export default function Bio() {
+  return (
+    <div className={BCss.mDiv}>
+      <p className={BCss.pTagMain}></p>
+    </div>
+  );
+}
