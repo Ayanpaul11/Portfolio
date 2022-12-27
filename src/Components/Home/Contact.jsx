d4877b19f8eeb7250d3c6794f3b8103a1c32ccd4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 23:52:12 +0530

    Update Contact.jsx

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -5,8 +5,6 @@ import CCss from "./Css/Contact.module.css";
 export default function Contact() {
   return (
     <div className={CCss.mDiv}>
-      {/* <p className={CCss.GreyText}>VARSITY</p> */}
-
       <div className={CCss.mConDiv}>
         <div>
           <p className={CCss.ContactMe}>Contact Me</p>
