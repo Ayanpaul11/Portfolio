4c1521e95e70eccee01c3a7a8059271f9e9f7575
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-28 00:08:01 +0530

    Update Contact.jsx

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import Map from "./../../assets/Mapsicle Map.png";
 import CCss from "./Css/Contact.module.css";
+import ContactData from "./../../Data/Contact.json";
+import Map from "./../../assets/Mapsicle Map.png";
+import user from "./../../assets/user.png";
+import phoneCall from "./../../assets/phone-call.png";
+import mail from "./../../assets/mail.png";
 
 export default function Contact() {
   return (
@@ -8,6 +12,37 @@ export default function Contact() {
       <div className={CCss.mConDiv}>
         <div>
           <p className={CCss.ContactMe}>Contact Me</p>
+          {ContactData ? (
+            <>
+              {ContactData.map((value, key) => {
+                return (
+                  <div key={key}>
+                    {/* name */}
+                    <div className={CCss.contactMapDivChild}>
+                      <img src={user} alt="" className={CCss.contactIcon} />
+                      <p className={CCss.contactDivPTag}>{value.name}</p>
+                    </div>
+                    {/* phoneNumber */}
+                    <div className={CCss.contactMapDivChild}>
+                      <img
+                        src={phoneCall}
+                        alt=""
+                        className={CCss.contactIcon}
+                      />
+                      <p className={CCss.contactDivPTag}>{value.phoneNumber}</p>
+                    </div>
+                    {/* email */}
+                    <div className={CCss.contactMapDivChild}>
+                      <img src={mail} alt="" className={CCss.contactIcon} />
+                      <p className={CCss.contactDivPTag}>{value.email}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            ""
+          )}
         </div>
         <img src={Map} alt="" className={CCss.MapImg} />
       </div>
