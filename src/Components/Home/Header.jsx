efe6b67de16055360415e7dfbb101391e1ea7dd1
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 16:28:56 +0530

    Update Header.jsx

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import HCss from "./Css/Header.module.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return <div className={HCss.mDiv}></div>;
 }
