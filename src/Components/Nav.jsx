c977adfa77aab07fb92931750d71e8730677dd79
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 16:22:04 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import NCss from "./Css/Nav.module.css";
 
-export default function Nav() {
+export default function Nav(props) {
   return (
     <div className={NCss.mDiv}>
-      <p className={NCss.pTagHead}>My Portfolio</p>
+      <p className={NCss.pTagHead}>{props.leftTitle}</p>
       <div className={NCss.hamburger}>
         <div className={NCss.bur1}></div>
         <div className={NCss.bur2}></div>
