c64d90f084a6b9ccb9e2fecf21facf6c590219e3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 17:59:27 +0530

    Update Bio.jsx

--- src/Components/Home/Bio.jsx
+++ src/Components/Home/Bio.jsx
@@ -5,6 +5,11 @@ export default function Bio() {
   return (
     <div className={BCss.mDiv}>
       <p className={BCss.pTagMain}>Me, Myself & I</p>
+      <p className={BCss.desPTag}>
+        A small gallery of recent projects chosen by me. I've done them all
+        together with amazing people from companies around the globe. It's only
+        a drop in the ocean compared to the entire list.
+      </p>
     </div>
   );
 }
