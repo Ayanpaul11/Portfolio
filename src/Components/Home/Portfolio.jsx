25bd626776a8560184ed13868c088eb8a4d4b9a6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 19:50:49 +0530

    Update Portfolio.jsx

--- src/Components/Home/Portfolio.jsx
+++ src/Components/Home/Portfolio.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PCSS from "./Css/Portfolio.module.css";
 
 export default function Portfolio() {
+  const [show, set] = useState("GRAPHIC");
+
+  useEffect(() => {
+    console.log(show);
+  }, [show]);
+
   return (
     <div>
       <div className={PCSS.herderDiv}>
         <div className={PCSS.btnDivP}>
-          <div className={PCSS.GRAPHIC}>GRAPHIC</div>
-          <div className={PCSS.APP}>APP</div>
-          <div className={PCSS.WEB}>WEB</div>
+          <div
+            className={PCSS.GRAPHIC}
+            id={show === "GRAPHIC" ? "selected" : "NotSelected"}
+            onClick={() => set("GRAPHIC")}
+          >
+            GRAPHIC
+          </div>
+          <div className={PCSS.APP} onClick={() => set("APP")}>
+            APP
+          </div>
+          <div className={PCSS.WEB} onClick={() => set("WEB")}>
+            WEB
+          </div>
         </div>
       </div>
       Portfolio
