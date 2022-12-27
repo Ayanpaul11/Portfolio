1d6f8b7b36b044ed35d05255a7b9053c8361afe5
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-28 01:04:28 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import FCSS from "./Css/Footer.module.css";
 
 export default function Footer() {
-  return (
-    <div className={FCSS.mDiv}>
-      <div className={FCSS.line}></div>
-      <p className={FCSS.lastLine}>Made with 💖 by Ayan Paul</p>
-    </div>
-  );
+  return <div>Footer</div>;
 }
