2a9102412da79e845e811d27d39cd47e8e8489ff
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-28 00:26:15 +0530

    Create Socials.jsx

---
+++ src/Components/Home/Socials.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CCss from "./Css/Contact.module.css";
+
+// icons
+import LinkedIn from "./../../assets/LinkedIn.png";
+import Twitter from "./../../assets/Twitter.png";
+import Github from "./../../assets/Github.png";
+
+export default function Socials() {
+  return <div></div>;
+}
