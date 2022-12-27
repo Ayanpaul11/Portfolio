b27b0201ec102063518ef1a75e4b852db2a6ef16
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 23:38:13 +0530

    Update Contact.jsx

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import CCss from "./Css/Contact.module.css";
 
 export default function Contact() {
-  return <div className={CCss.mDiv}>Contact</div>;
+  return (
+    <div className={CCss.mDiv}>
+      <div className={CCss.mConDiv}>
+        <div>
+          <p>Contact Me</p>
+        </div>
+        <div>map</div>
+      </div>
+    </div>
+  );
 }
