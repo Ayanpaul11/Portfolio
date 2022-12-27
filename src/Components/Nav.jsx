26a7c27bde3e9e1e2b24da1201226fd0ee716afe
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 16:06:52 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,11 @@ export default function Nav() {
   return (
     <div className={NCss.mDiv}>
       <p className={NCss.pTagHead}>My Portfolio</p>
-      <div className="hamburger">Hello</div>
+      <div className="hamburger">
+        <div className="bur1"></div>
+        <div className="bur2"></div>
+        <div className="bur3"></div>
+      </div>
     </div>
   );
 }
