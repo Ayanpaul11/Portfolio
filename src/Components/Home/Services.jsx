ab412a2c0898c1fb45c4f1594ed1d9d88c0d39f8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 23:03:09 +0530

    Create Services.jsx

---
+++ src/Components/Home/Services.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SCss from "./Css/Services.module.css";
+
+export default function Services() {
+  return (
+    <div className={SCss.mDiv}>
+      <p className={SCss.GreyText}>VARSITY</p>
+      <br />
+      Services com
+    </div>
+  );
+}
