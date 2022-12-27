6d7bdc59d978572ae237827902df3a4d29d6ee51
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-27 19:39:43 +0530

    Update Portfolio.jsx

--- src/Components/Home/Portfolio.jsx
+++ src/Components/Home/Portfolio.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import PCSS from "./Css/Portfolio.module.css";
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <div className={PCSS.herderDiv}>
+        <div className={PCSS.btnDivP}>
+          <div className={PCSS.GRAPHIC}>GRAPHIC</div>
+          <div className={PCSS.APP}>APP</div>
+          <div className={PCSS.WEB}>WEB</div>
+        </div>
+      </div>
+      Portfolio
+    </div>
+  );
 }
